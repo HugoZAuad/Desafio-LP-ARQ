406c7f3ddb28bdf23e13646e440741213b731fa3
HugoZAuad <hugozeymer@hotmail.com>
2024-09-19 00:41:55 -0300

    Primeira versão atualizada

---
+++ index.js
@@ -0,0 +1,56 @@
+var Imag1 = window.document.getElementById("img1")
+var Imag2 = window.document.getElementById("img2")
+var Imag3 = window.document.getElementById("img3")
+var Imag4 = window.document.getElementById("img4")
+var Imag5 = window.document.getElementById("img5")
+var Imag6 = window.document.getElementById("img6")
+
+function MudarImagem1(){
+    Imag1.style = "Display:none"
+    Imag2.style = "Display:flex"
+    Imag3.style = "Display:none"
+    Imag4.style = "Display:none"
+    Imag5.style = "Display:none"
+    Imag6.style = "Display:none"
+}
+function MudarImagem2(){
+    Imag1.style = "Display:none"
+    Imag2.style = "Display:none"
+    Imag3.style = "Display:flex"
+    Imag4.style = "Display:none"
+    Imag5.style = "Display:none"
+    Imag6.style = "Display:none"
+}
+function MudarImagem3(){
+    Imag1.style = "Display:none"
+    Imag2.style = "Display:none"
+    Imag3.style = "Display:none"
+    Imag4.style = "Display:flex"
+    Imag5.style = "Display:none"
+    Imag6.style = "Display:none"
+}
+function MudarImagem4(){
+    Imag1.style = "Display:none"
+    Imag2.style = "Display:none"
+    Imag3.style = "Display:none"
+    Imag4.style = "Display:none"
+    Imag5.style = "Display:flex"
+    Imag6.style = "Display:none"
+}
+function MudarImagem5(){
+    Imag1.style = "Display:none"
+    Imag2.style = "Display:none"
+    Imag3.style = "Display:none"
+    Imag4.style = "Display:none"
+    Imag5.style = "Display:none"
+    Imag6.style = "Display:flex"
+}
+function MudarImagem6(){
+    Imag1.style = "Display:flex"
+    Imag2.style = "Display:none"
+    Imag3.style = "Display:none"
+    Imag4.style = "Display:none"
+    Imag5.style = "Display:none"
+    Imag6.style = "Display:none"
+}
+
